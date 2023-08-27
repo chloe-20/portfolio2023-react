3ba7f4989a263e32d6557a5d4bc2501698454952
chloe iy choi <ioiuioiciy88@gmail.com>
2023-08-27 19:28:06 +0900

    연락처 완료

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,7 +1,38 @@
 import React from "react";
 
 const Contact = () => {
-  return <div>Contact</div>;
+  return (
+    <section id="contact">
+      <div className="contact__inner">
+        <h2 className="contact__title">CONTACT</h2>
+        <div className="contact__lines top" aria-hidden="true">
+          <span className="line"></span>
+          <span className="line"></span>
+          <span className="line"></span>
+          <span className="line"></span>
+          <span className="line"></span>
+          <span className="line"></span>
+          <span className="line"></span>
+        </div>
+        <div className="contact__text">
+          <div className="text">                        
+            <div>
+              <a href="mailto:202.worker@gmail.com" rel="noopener  noreferrer" target="_blank">MAIL : 202.worker@gmail.com</a>
+            </div>
+          </div>
+        </div>
+        <div className="contact__lines bottom" aria-hidden="true">
+          <span className="line"></span>
+          <span className="line"></span>
+          <span className="line"></span>
+          <span className="line"></span>
+          <span className="line"></span>
+          <span className="line"></span>
+          <span className="line"></span>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Contact;
