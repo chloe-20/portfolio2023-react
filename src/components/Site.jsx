d6b8e4ca42703cf5ab3fb54bf7582866a1ccd8af
chloe iy choi <ioiuioiciy88@gmail.com>
2023-08-27 18:58:19 +0900

    사이트 완료

--- src/components/Site.jsx
+++ src/components/Site.jsx
@@ -1,7 +1,66 @@
 import React from "react";
 
+const siteText = [
+  {
+    text: ["MAKE","SITE COMPLIANT WITH","WEBSTANDARD",],
+    title: "웹 표준을 준수한 사이트 제작",
+    code: "https://github.com/chloe-20/portfolio2023-vite",
+    view: "https://portfolio2023-vite.netlify.app/",
+    info: ["SITE CODING","PRODUCTION PERIOD : TWO DAYS","USE STACK : HTML5/CSS3, CSS VARIABLE, VITE",],    
+  },
+  {
+    text: ["MAKE","SITE COMPLIANT WITH","REACT.JS",],
+    title: "리액트를 이용한 사이트 제작",
+    code: "https://github.com/chloe-20/portfolio2023-react",
+    view: "https://portfolio2023-react.netlify.app/",
+    info: ["SITE CODING","PRODUCTION PERIOD : TWO DAYS","USE STACK : HTML5/CSS3, CSS VARIABLE, REACT.JS",],
+  },
+  {
+    text: ["MAKE","SITE COMPLIANT WITH","VUE.JS",],
+    title: "뷰를 이용한 사이트 제작",
+    code: "https://github.com/chloe-20/portfolio2023-vue",
+    view: "https://portfolio2023-vue.netlify.app/",
+    info: ["SITE CODING","PRODUCTION PERIOD : TWO DAYS","USE STACK : HTML5/CSS3, CSS VARIABLE, VUE.JS",],
+  },
+  {
+    text: ["MAKE","SITE COMPLIANT WITH","NEXT.JS",],
+    title: "넥스트를 이용한 사이트 제작",
+    code: "https://github.com/chloe-20/portfolio2023-next",
+    view: "https://portfolio2023-next.netlify.app/",
+    info: ["SITE CODING","PRODUCTION PERIOD : TWO DAYS","USE STACK : HTML5/CSS3, CSS VARIABLE, NEXT.JS",],
+  },
+];
+
 const Site = () => {
-  return <div>Site</div>;
+  return (
+    <section id="site">
+      <div className="site__inner">
+        <h2 className="site__title">SITE CODING <em>나의 작업물</em></h2>
+        <div className="site__wrap">
+          {siteText.map((site, key) => (
+            <article className={`site__item s${key+1}`} key={key}>
+              <span className="num">{key+1}.</span>
+              <div className="text">
+                <div>{site.text[0]}</div>
+                <div>{site.text[1]}</div>
+                <div>{site.text[2]}</div>
+              </div>
+              <h3 className="title">{site.title}</h3>
+              <div className="btn">
+                <a href={site.code}>CODE</a>
+                <a href={site.view}>VIEW</a>
+              </div>
+              <div className="info">
+                <span>{site.info[0]}</span>
+                <span>{site.info[1]}</span>
+                <span>{site.info[2]}</span>
+              </div>
+            </article>
+          ))}          
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Site;
